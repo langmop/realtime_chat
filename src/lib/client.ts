1b4d6ea22e3d8661295fbce74bda466a7a38d454
Langmop <langmop@Gauravs-MacBook-Pro-2.local>
2025-12-31 13:40:28 +0530

    updated the base url key

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,5 +2,5 @@ import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
 // .api to enter /api prefix
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+const API_BASE_URL = process.env.VERCEL_URL || "http://localhost:3000";
 export const client = treaty<typeof App>(API_BASE_URL).api;

d6817859568751e27d20fac3a9d840f94e3f25a4
Langmop <langmop@Gauravs-MacBook-Pro-2.local>
2025-12-31 13:32:44 +0530

    [fix]: Removed unused files

--- src/lib/client (2).ts
+++
@@ -1,6 +0,0 @@
-import { treaty } from "@elysiajs/eden";
-import type { App } from "../app/api/[[...slugs]]/route";
-
-// .api to enter /api prefix
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
-export const client = treaty<typeof App>(API_BASE_URL).api;
--- src/lib/realtime-client (1).ts
+++
@@ -1,6 +0,0 @@
-"use client";
-
-import { createRealtime } from "@upstash/realtime/client";
-import type { RealTimeEvents } from "./realtime";
-
-export const { useRealtime } = createRealtime<RealTimeEvents>();
